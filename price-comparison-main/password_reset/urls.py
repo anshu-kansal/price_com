0f48e137699143e9b6e4cef346692e519ce3a42a
Khushi Mehta <151489411+Khushimehta18@users.noreply.github.com>
2026-03-01 21:34:48 +0530

    Delete price-comparison-main directory

--- price-comparison-main/password_reset/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('request/', views.password_reset_request, name='password-reset-request'),
-    path('verify/', views.password_reset_verify, name='password-reset-verify'),
-    path('reset/', views.password_reset_confirm, name='password-reset-confirm'),
-]
